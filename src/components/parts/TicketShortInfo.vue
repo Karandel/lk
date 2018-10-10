6e781992c8a2d6004639504bc064375da9c576dd
Tolokonnikov Dmitriy <Dmitriy.Tolokonnikov@alp.ru>
2018-10-10 16:18:17 +0300

    Card styles

--- src/components/parts/TicketShortInfo.vue
+++ src/components/parts/TicketShortInfo.vue
@@ -1,15 +1,19 @@
 <template>
   <v-flex xs12>
-    <v-card class="clickable" tile flat >
-      <v-card-title primary-title>
-        <div>
-          <span>{{ticket.number}}</span><br>
-          <span>{{ticket.status}}</span><br>
-          <span>{{ticket.type}}</span><br>
-          <span>Крайний срок: {{ticket.deadlineDate | moment($defaultDateTimeFormat)}}</span><br>
-          <span v-html="ticket.title"></span>
-        </div>
-      </v-card-title>
+    <v-card class="clickable cardElementOfList" hover>
+      <v-card-text>
+        <v-layout row wrap>
+          <v-flex md8>
+            <span>{{ticket.number}} от {{ticket.creationDate | moment($defaultDateTimeFormat)}}</span><br>
+            <h2 v-html="ticket.title"></h2>
+          </v-flex>
+          <v-flex md4>
+            <b><span>Статус: {{ticket.status}}</span></b><br>
+            <span>Инициатор: {{ticket.requestor}}</span><br>
+            <span>Крайний срок: {{ticket.deadlineDate | moment($defaultDateTimeFormat)}}</span>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
     </v-card>
   </v-flex>
 </template>
@@ -17,6 +21,11 @@
 <style scoped>
   .clickable {
     cursor: pointer;
+  }
+  .cardElementOfList {
+    margin: 5px;
+    border-radius: 10px;
+    border-left: 6px solid green;
   }
 </style>
 
